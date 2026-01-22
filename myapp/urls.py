b47a5912295fc00d28aa82dcae84a3d0125d45e2
sreeragrajan2003-ctrl <sreeragrajan2003@gmail.com>
2026-01-22 10:40:05 +0530

    initial django project

---
+++ myapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/',views.loginPage,name="login"),
+    path('register/',views.registerPage,name="register"),
+    path('logout/',views.logoutUser,name="logout"),
+    path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>/',views.userProfile,name='user-profile'),
+    path('create-rooms/',views.CreateRoom,name="create-room"),
+    path('update-rooms/<str:pk>/',views.UpdateRoom,name="update-room"),
+    path('delete-rooms/<str:pk>/',views.DeleteRoom,name="delete-room"),
+    path('delete-message/<str:pk>/',views.deleteMessage,name="delete-message"),
+]
